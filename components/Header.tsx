d2269678d29b257c707ca94711a9514eabc5b394
Kyoze <hrmnfrmnsyh1210@gmail.com>
2026-02-01 16:12:46 +0700

    update header

--- components/Header.tsx
+++ components/Header.tsx
@@ -120,6 +120,14 @@ export const Header: React.FC<HeaderProps> = ({ onNavigate }) => {
               {link.name}
             </a>
           ))}
+
+          <Button
+            variant="primary"
+            className="w-full mt-4"
+            onClick={() => handleNavClick("#contact")}
+          >
+            Mulai Proyek
+          </Button>
           <Button
             variant="ghost"
             className="w-full justify-start text-slate-300"
@@ -130,13 +138,6 @@ export const Header: React.FC<HeaderProps> = ({ onNavigate }) => {
             leftIcon={<LogIn className="w-4 h-4" />}
           >
             Login Admin
-          </Button>
-          <Button
-            variant="primary"
-            className="w-full mt-4"
-            onClick={() => handleNavClick("#contact")}
-          >
-            Mulai Proyek
           </Button>
         </div>
       )}
